ba72e128141fb3bc57b85ca61804ea43bac7719b
Lazlo <lopezslazarus@gmail.com>
2021-07-25 14:26:19 -0700

    General Function Updates

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -56,28 +56,40 @@ const getNeighborsOf = ([x, y]) => [
 ];
 
 const getLivingNeighbors = (cell, state) => {
-
-
+  return getNeighborsOf(cell).filter((n) => contains.bind(state)(n));
 };
 
 const willBeAlive = (cell, state) => {
-
+  const livingNeighbors = getLivingNeighbors(cell, state);
+  return (
+    livingNeighbors.length === 3 ||
+    (contains.call(state, cell) && livingNeighbors.length === 2)
+  );
 
 };
 
 const calculateNext = (state) => {
-
-
+  const { bottomLeft, topRight } = corners(state);
+  let result = [];
+  for (let y = topRight[1] + 1; y >= bottomLeft[1] - 1; y--) {
+    for (let x = bottomLeft[0] - 1; x <= topRight[0] + 1; x++){
+      result = result.concat(willBeAlive([x,y], state) ? [[x, y]] : []);
+    }
+  }
+  return result; 
 };
 
 const iterate = (state, iterations) => {
-
-
+  const states = [state];
+  for (let i = 0; i <iterations; i++) {
+    states.push(calculateNext(states[states.length - 1]));
+  }
+  return states; 
 };
 
 const main = (pattern, iterations) => {
-
-
+  const results = iterate(startPatterns[pattern], iterations);
+  results.forEach(r => console.log(printCells(r)));
 };
 
 const startPatterns = {
